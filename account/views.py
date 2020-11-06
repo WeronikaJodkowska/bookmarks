528a4ba29f8d19817d9a65bc235dcfc74f84e95a
weronika <veronica.jodkowska@gmail.com>
2020-11-06 01:07:39 -0800

    Django authentication views

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -10,9 +11,9 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user =  authenticate(request,
-                                 username=cd['username'],
-                                 password=cd['password'])
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -24,3 +25,10 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
