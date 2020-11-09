23f8044a6bd0e05af54b13a87aad668385651623
WeronikaJodkowska <veronica.jodkowska@gmail.com>
2020-11-09 02:29:24 -0800

    Building a custom authentication backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -136,3 +136,8 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
