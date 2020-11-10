7dfcc478b1cfe9fc8f3c71e889ff6190ec93985f
WeronikaJodkowska <veronica.jodkowska@gmail.com>
2020-11-10 05:54:18 -0800

    Displaying the activity stream

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 
 from common.decorators import ajax_required
 from actions.utils import create_action
+from actions.models import Action
 
 from .forms import LoginForm, UserRegistrationForm, \
     UserEditForm, ProfileEditForm
@@ -37,9 +38,19 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions[:10]
+
+        return render(request,
+                      'account/dashboard.html',
+                      {'section': 'dashboard',
+                       'actions': actions})
 
 
 def register(request):
